fd32da4f765c9cc871d1cc38b1769b0b2052cc60
shaoyong <shaoyong@infohole.com.cn>
2016-05-23 15:42:39 +0800

    7

--- conf.py
+++ conf.py
@@ -36,6 +36,12 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser
+}
+
 source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
