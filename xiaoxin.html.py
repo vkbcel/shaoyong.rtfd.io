3065bc5f8bdffb2d4ae8e9b6bf61848961e1e04e
shaoyong <shaoyong@infohole.com.cn>
2016-06-08 15:55:09 +0800

    6-8

---
+++ xiaoxin.html.py
@@ -0,0 +1,50 @@
+XXXX XXXXXXXXXXXXXXXXXXXXXXX
+                                                                                                                        
+                                                                                                                        
+                                              XXX                     XXX                                               
+                                            XXXXXXX XXXXXXXXXXXXXXXXXXXXXXX                                             
+                                          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                                            
+                                         XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                                           
+                                        XXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXXX                                         
+                                       XXXXXXXXXXXXXX X      XXXXXXXXXXXXXXXXXXXX                                       
+                                      XXXXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXX                                     
+                                     XXXXXXXXXXXXXXXXXX    XXXXXXXXXX   XXXXXXXXXXXX                                    
+                                    XXXXXXXXX   XXXXXXX    XXXXXXXXX     XXXXXXXXXXXXX                                  
+                                    XXXXXXXXXXXXXXXXXXX    XXXXXXXX     XXXXXXXXXXXXXXX                                 
+                                   XXXXXXXXXX XXXXXXXXX    XXXXXXXX XXXXXXXXXXXXXXXXXXXX                                
+                                   XXXXXXX       XXXXX     XXXXXX XXX      XXXXXXXXXXXXXX                               
+                                  XXXXXXXX         XX      XXXXX XX          XXXXXXXXXXXXX                              
+                                  XX XXX           XX         X XX             XXX   XXXXXX                             
+                                 XXXXX  X     X     XX         XX        X      XX    XXXXXX                            
+                                 XXXX   XXXXXXXXX    X        XXXX XXXXXXXX      XX    XXXXXX                           
+                                XX XXXXXXXXXXXXXXX X X       XX  XXXXXXXXXXXXX    X    XXXXXX                           
+                                XXXX XXXXXXXXXXXXXXX XX      X  XXXXXXXXXXXXXXX   XX   XXXXXXX                          
+                                X   XXXXXXXXXXXXXXXXXXX      XXXXXXXXXXXXXXXXXXX   X    XXXXXXX                         
+                               XX   XXXXXXXXXXXXXXXXX         XXXXXXXXXXXXXXXXXX   XX   XXXXXXX                         
+                               XX   XXXXXXXXXXXXXXXXX        XXXXXXXXX XXXXXXXXX    X   XXXXXXXX                        
+                               XX  XXXXXXXX  XXXXXXXXX       XXXXXXXX  XXXXXXXXXX   XX   XXXXXXX                        
+                              XX   XXXXXXXX   XXXXXXXX       XXXXXXX    XXXXXXXXX   XX   XXXXXXXX                       
+                       XXXXXXXX    XXXXXXX    XXXXXXXX       XXXXXX       XXXXXXXX   X   XXXXXXXXXXXXX                  
+                    XXXXXXX  X     XXXXXXX   XXXXXXXXX       XXXXXX     XXXXXXXXX    X    XXXXXXXX XXXX                 
+                  XXXX             XXXXXXX  XXXXXXXXXXX      XXXXXXX   XXXXXXXXXX         XXXXXX     XXX                
+                 XXXX              XXXXXXXX XXXXXXXXXXX      XXXXXXX  XXXXXXXXXXX         XXXXX       XXX               
+               XXXX                 XXXXXXX XXXXXXXXXXX      XXXXXXXX XXXXXXXXXXX         XXXXX         XX              
+              XXX                   XXXXXXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXXXXX          XXX          XX              
+             XXX                     XXXXXXXXXXXXXXXX         XXXXXXXXXXXXXXXXXXX          XX            XX             
+            XX                        XXXXXXXXXXXXXX           XXXXXXXXXXXXXXXX X          X              XX            
+           XXX                          XXXXXXXXXX             XXXXXXXXXXXXXXXX            X              XX            
+          XXX        XXX                 XXXXXXXX               XXXXXXXXXXXXXX             X               XX           
+          XX   XX   X XX                                           XXXXXXXXXX                              XX           
+         XXX  XXXXXXXXX XX                                             XX X                                XXX          
+         XX    XX  XXXXXX XX                                          XXXXXXX                               XX          
+        XXX   XX  XX XXX  XX                                          XXXXX X    X                          XX          
+        X X  XX XXX XXX  XX                                       XX  XX XX XX   XX                         XX          
+       XX    X  XXX XX                                           XX  XXXXX XXX XXX                           X          
+       XX       X  XX                                           XX  XX X  XXX XXX                            X          
+      XX        XX  XX                                          X XXX   XXX   XX                             XX         
+      XX        XX                                                 X X  XX                                   XX         
+      XX                                                                  XX                                 XX         
+      XX                                                                                                     X          
+      XX                                                                                                     X          
+      X                                                                                                     XX          
+XXXXXX
